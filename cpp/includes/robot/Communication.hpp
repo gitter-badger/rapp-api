ca5f78a66fe5dd8cfbba49078dcbbabdc2b9225c
jfigat <jan.figat@gmail.com>
2015-08-07 12:21:16 +0200

    API - functions update

--- cpp/includes/robot/Communication.hpp
+++ cpp/includes/robot/Communication.hpp
@@ -4,6 +4,8 @@
 
 #include <string>
 
+#include <vector>
+
 /**
  * @class Communication
  * @brief Class which defines the interface for Robot communication capabilities (speaking, listening)
@@ -32,8 +34,26 @@ class Communication
     /// Produce Audio from robot's speakers
     bool playAudio(const std::string & file_path, double position, double volume, double balance, bool play_in_loop); 
     
+    /// Say given sentence in default language
+    bool textToSpeech(const std::string & str);
+    
     /// Say given sentence in selected language
     bool textToSpeech(const std::string & str, Language language = Language::ENGLISH);
+    
+    /// Recognize the word included in the database
+    std::string wordSpotting(std::string dictionary[], int size);
+    
+    /// Record the audio message from the microphones by the desired time
+	std::string captureAudio(int time);
+	
+	/// Record the audio message with silence detection
+	std::string captureAudio(std::string & file_path, float waiting_time, int microphone_energy);
+	
+	/// Give the detected energy by the selected microphone
+	int microphoneEnergy(std::string & name);
+	
+	/// Record the audio message to the buffer
+	void voiceRecord(bool startRecording, std::vector< std::vector<unsigned char> > &audio_buffer_vector);
 
   private:
     CommunicationImpl * pimpl;
