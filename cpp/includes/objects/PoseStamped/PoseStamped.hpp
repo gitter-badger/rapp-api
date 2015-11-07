76fcbcd79b553f26fb45157257e7c34c91b7ce10
dudekw <wojciechsbox@gmail.com>
2015-11-08 00:06:20 +0100

    general update, Navigaation structure distribution

--- cpp/includes/objects/PoseStamped/PoseStamped.hpp
+++ cpp/includes/objects/PoseStamped/PoseStamped.hpp
@@ -9,11 +9,30 @@ namespace rapp {
 
 		class PoseStamped
 		{
+			Header header_def;
+		
+			header_def.seq = 0;
+			header_def.frame_id = "/map";
+			header_def.stamp.sec = 0;
+			header_def.stamp.nsec = 0;
+			Pose pose_def;
+			
+			pose_def.header = header_def;
+			pose_def.pose.position.x = 0;
+			pose_def.pose.position.y = 0;
+			pose_def.pose.position.z = 0;
+
+			pose_def.pose.orientation.x = 0;
+			pose_def.pose.orientation.y = 0;	
+			pose_def.pose.orientation.z = 0;
+			pose_def.pose.orientation.w = 1;
 			public:
 			Pose pose;
 			Header header;
-			PoseStamped(Pose pose_=pose, Header header_=header):pose(pose_),header(header_){}
+			PoseStamped(Pose pose_=pose_def, Header header_=header_def):pose(pose_),header(header_){}
 			PoseStamped () = default;
+
+
 		};
 
 	}
