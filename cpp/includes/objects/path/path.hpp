98502c201ab1e9c40ffed85b081a47f10bc9354e
dudekw <wojciechsbox@gmail.com>
2015-11-09 17:48:51 +0100

    change navigation objects names

--- cpp/includes/objects/path/path.hpp
+++ cpp/includes/objects/path/path.hpp
@@ -1,7 +1,7 @@
 #ifndef RAPP_OBJECT_PATH
 #define RAPP_OBJECT_PATH
-#include "../PoseStamped/PoseStamped.hpp"
-#include "../Msg_header/Header.hpp"
+#include "../poseStamped/poseStamped.hpp"
+#include "../msg_header/header.hpp"
 #include <vector>
 namespace rapp {
 	namespace object {
